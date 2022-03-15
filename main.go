e4b47e2f39c882a4ec09b0eaf9f73c0e50fd32d7
Alexis Provo <alexis.provo@ynov.com>
2022-03-15 16:00:29 +0100

    New routes

--- main.go
+++ main.go
@@ -44,30 +44,8 @@ var p = Page{
 	Artists: &artist,
 }
 
-
 func main() {
 	artist = GetArtists()
-
-
-	for i := range artist {
-		datesLocations := GetRelation(artist[i].Relations).DatesLocation
-		locations := GetLocations(artist[i].Locations).Locations
-		locations = removeDuplicateStr(locations)
-		fmt.Printf("ID = %v\n", artist[i].Id)
-		fmt.Printf("Name = %s\n", artist[i].Name)
-		fmt.Printf("Creation date = %d\n", artist[i].CreationDate)
-		fmt.Printf("Members = %+q\n", artist[i].Members)
-		fmt.Printf("First Album = %s\n", artist[i].FirstAlbum)
-		fmt.Printf("Locations URL = %s\n", artist[i].Locations)
-		fmt.Printf("Dates URL = %s\n", artist[i].ConcertDates)
-		fmt.Printf("Relations URL = %s\n", artist[i].Relations)
-		fmt.Printf("Locations = %+q\n", GetLocations(artist[i].Locations).Locations)
-		for _, location := range locations {
-			fmt.Printf("Location = %s | Dates = %+q\n", location, datesLocations[location])
-		}
-		fmt.Printf("Image URL = %s\n\n", artist[i].Image)
-		imagesURLs = append(imagesURLs, artist[i].Image)
-	}
 
 	fs := http.FileServer(http.Dir("templates"))
 	router := http.NewServeMux()
@@ -75,8 +53,10 @@ func main() {
 	//Create a server and listen on port 8080
 	fmt.Println("Starting server on port 8080")
 
-	//Serve template for the homepage
-	router.HandleFunc("/", HandlerHomepage)
+	//Serve template for pages
+	router.HandleFunc("/", HandlerIndex)
+	router.HandleFunc("/homepage", HandlerHomepage)
+	router.HandleFunc("/profile", HandlerProfile)
 
 	//Handle requests for files in /templates (ex : style.css)
 	router.Handle("/templates/", http.StripPrefix("/templates/", fs))
